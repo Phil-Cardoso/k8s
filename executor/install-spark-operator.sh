b49716614249d2f67f22390ae37c74e12e5fc224
Phil-Cardoso <phillip.cardoso@hotmail.com>
2025-08-13 23:54:16 -0300

    fix: executor em default

--- executor/install-spark-operator.sh
+++ executor/install-spark-operator.sh
@@ -8,8 +8,15 @@ helm install spark-operator spark-operator/spark-operator \
   --set webhook.enable=true \
   --set serviceAccounts.spark.name=spark \
   --set enableBatchScheduler=true \
-  --set batchScheduler.enable=true
+  --set batchScheduler.enable=true \
+  --wait
 
 kubectl create clusterrolebinding spark-cleanup \
   --clusterrole=edit \
   --serviceaccount=executor:spark
+
+kubectl apply -f spark-operator-rolebinding.yaml
+kubectl apply -f spark-cleanup-rolebinding.yaml
+kubectl apply -f spark-cleanup-role.yaml
+kubectl apply -f RoleBinding.yaml
+kubectl apply -f role.yaml
